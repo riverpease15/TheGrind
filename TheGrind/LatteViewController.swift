cbd75c1c337ed5bf87a18c86c6d372e09668da61
Hunter Malinowski <huntermalinowski@gmail.com>
2020-07-28 09:01:47 -0700

    Added View Controllers

---
+++ TheGrind/LatteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LatteViewController.swift
+//  TheGrind
+//
+//  Created by Apple on 7/28/20.
+//  Copyright © 2020 River Pease. All rights reserved.
+//
+
+import UIKit
+
+class LatteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
